5fbcca7874ffd965e0dc3a70479e38d738de712d
Hossein Mansouri <hsnmsri82@gmail.com>
2024-09-11 15:56:58 +0330

    update uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -13,13 +13,6 @@ sudo systemctl daemon-reload
 # Remove the Linux Monitoring binary and directory
 sudo rm -rf /opt/linux_monitoring
 
-# Uninstall tgbot-cpp
-sudo rm -rf /opt/tgbot
-sudo make uninstall -C /opt/tgbot || echo "tg-bot may have already been uninstalled"
-
-# Uninstall dependencies
-sudo apt remove --purge -y wget unzip g++ make binutils cmake libboost-system-dev libssl-dev zlib1g-dev libcurl4-openssl-dev nlohmann-json3-dev
-
 # Clean up residual configuration and dependencies
 sudo apt autoremove -y
 sudo apt autoclean
