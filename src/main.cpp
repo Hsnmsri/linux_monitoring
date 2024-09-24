dd5c1f038f5c9825f9ff0c63b849370309101322
Hossein Mansouri <hsnmsri82@gmail.com>
2024-09-24 14:22:57 +0330

    update app flow and change thread status

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,24 @@
 #include <MemoryMonitor.hpp>
 #include <TelegramMonitor.hpp>
 
+int app();
+
 int main()
+{
+    try
+    {
+        return app();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+
+    return 0;
+}
+
+int app()
 {
     // Monitoring Status
     bool isMonitoringEnable = false;
@@ -17,28 +34,37 @@ int main()
     if (!settings.getSetting())
     {
         logger.logToConsole("Failed to set settings!");
-        return 0;
+        return 1;
     }
 
     logger.logToConsole("Linux Monitoring v" + settings.getAppVersion() + " Service Started");
     logger.logToConsole(settings.getServerName());
     // Monitoring Objects
-    CpuMonitor cpu(settings.getCpuCheckDuration(), isMonitoringEnable);
-    MemoryMonitor memory(settings.getMemoryCheckDuration(), isMonitoringEnable);
+    CpuMonitor cpu(settings.getCpuCheckDuration());
+    MemoryMonitor memory(settings.getMemoryCheckDuration());
     TelegramMonitor telegram(isMonitoringEnable, cpu, memory, settings, logger);
 
     // Start Monitoring
-    cpu.startMonitoring();
-    memory.startMonitoring();
-    telegram.startTelegramNotificationWatchThread();
     telegram.startTelegramRequestThread();
 
     // App holder
     while (true)
     {
-        sleep(60);
+        // Start Monitoring
+        if (isMonitoringEnable)
+        {
+            cpu.startMonitoring();
+            memory.startMonitoring();
+            telegram.startTelegramNotificationWatchThread();
+        }
+        // Stop Monitoring
+        if (!isMonitoringEnable)
+        {
+            cpu.stopMonitoring();
+            memory.stopMonitoring();
+            telegram.stopTelegramNotificationWatchThread();
+        }
+        sleep(1);
         continue;
     }
-
-    return 0;
 }
