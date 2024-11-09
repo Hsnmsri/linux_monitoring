7aa890ee39c60a33b60614151d2b3a543cdb5b5c
Hossein Mansouri <hsnmsri82@gmail.com>
2024-11-09 10:23:16 +0330

    remove install packages

---
+++ install_packages.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install -y wget
+sudo apt install -y libcurl4-openssl-dev
+sudo apt install -y make binutils libboost-system-dev libssl-dev zlib1g-dev
+sudo apt install -y nlohmann-json3-dev
+echo "Package install successfully!"
